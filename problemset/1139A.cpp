ffde9a1a8058817815f86c6f0cf6123d1ae256e6
cyy <cyyever@outlook.com>
2019-03-22 16:42:50 +0800

    add solution

---
+++ problemset/1139A.cpp
@@ -0,0 +1,27 @@
+/*!
+ * \file 1139A.cpp
+ *
+ * \brief http://codeforces.com/problemset/problem/1139/A
+ * \author cyy
+ * \date 2017-06-12
+ */
+
+#include <iostream>
+
+int main(void) {
+  size_t n;
+  size_t cnt=0;
+
+  std::cin >> n;
+  unsigned  char c;
+  for (size_t i=0;i<n;i++) {
+    
+    std::cin >> c;
+    if( (c-'0' )%2==0) {
+      cnt+=i+1;
+    }
+
+  }
+  std::cout << cnt << std::endl;
+  return 0;
+}
