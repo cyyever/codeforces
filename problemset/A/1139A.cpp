203888463b0addb1fb246897443b9dc066b2c714
cyy <cyyever@outlook.com>
2019-03-22 16:56:08 +0800

    增加cmake支持

--- problemset/A/1139A.cpp
+++ problemset/A/1139A.cpp
@@ -10,17 +10,16 @@
 
 int main(void) {
   size_t n;
-  size_t cnt=0;
+  size_t cnt = 0;
 
   std::cin >> n;
-  unsigned  char c;
-  for (size_t i=0;i<n;i++) {
-    
-    std::cin >> c;
-    if( (c-'0' )%2==0) {
-      cnt+=i+1;
-    }
+  unsigned char c;
+  for (size_t i = 0; i < n; i++) {
 
+    std::cin >> c;
+    if ((c - '0') % 2 == 0) {
+      cnt += i + 1;
+    }
   }
   std::cout << cnt << std::endl;
   return 0;
