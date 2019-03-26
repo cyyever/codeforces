f80ed107a62135cf6b643e51cd7f0f3ebe40e379
cyy <cyyever@outlook.com>
2019-03-26 15:57:27 +0800

    add solution

---
+++ problemset/A/1136A.cpp
@@ -0,0 +1,37 @@
+/*!
+ * \file 118A.cpp
+ *
+ * \brief http://codeforces.com/problemset/problem/118/A
+ * \author cyy
+ */
+
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  size_t n;
+  std::cin >> n;
+
+  std::vector<uint64_t> end_pages;
+  for (size_t i=0;i<n;i++) {
+    uint64_t s,e;
+      std::cin>>s>>e;
+      end_pages.emplace_back(e);
+  }
+  uint64_t mark;
+  std::cin>>mark;
+
+  for(auto end_page:end_pages) {
+    if(mark>end_page) {
+      n--;
+    } else {
+      break;
+    }
+  }
+
+  std::cout <<  n<< std::endl;
+  return 0;
+}
---
+++ problemset/C/1136C.cpp
@@ -0,0 +1,50 @@
+/*!
+ * \file 118A.cpp
+ *
+ * \brief http://codeforces.com/problemset/problem/118/A
+ * \author cyy
+ */
+
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  size_t n,m;
+  std::cin >> n>>m;
+
+  std::map<uint64_t,std::vector<uint64_t>> A_antidiagonals;
+  std::map<uint64_t,std::vector<uint64_t>> B_antidiagonals;
+
+  for(size_t i=0;i<n;i++) {
+    for(size_t j=0;j<m;j++) {
+      uint64_t a;
+      std::cin>>a;
+      A_antidiagonals[i+j].emplace_back(a);
+    }
+  }
+
+  for(size_t i=0;i<n;i++) {
+    for(size_t j=0;j<m;j++) {
+      uint64_t a;
+      std::cin>>a;
+      auto &antidiagonal=B_antidiagonals[i+j];
+      antidiagonal.emplace_back(a);
+      auto &A_antidiagonal=A_antidiagonals[i+j];
+      if(antidiagonal.size()==A_antidiagonal.size()) {
+        std::sort(A_antidiagonal.begin(),A_antidiagonal.end());
+        std::sort(antidiagonal.begin(),antidiagonal.end());
+        if(antidiagonal!=A_antidiagonal) {
+          std::cout<<"NO";
+          return 0;
+        }
+      }
+    }
+  }
+
+  std::cout<<"YES";
+  return 0;
+}
