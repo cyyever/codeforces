355a4ed8d2cbc8d24929078674a2b485dc9504a2
cyy <cyyever@outlook.com>
2019-03-27 13:47:22 +0800

    add solution

---
+++ problemset/D/1136D.cpp
@@ -0,0 +1,77 @@
+/*!
+ * \file 118A.cpp
+ *
+ * \brief http://codeforces.com/problemset/problem/118/A
+ * \author cyy
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <vector>
+#include <unordered_map>
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  size_t n,m;
+  std::cin >> n>>m;
+
+  if(m==0){
+    std::cout<<0;
+    return 0;
+  }
+
+  std::vector<uint32_t> pupils;
+  pupils.reserve(n);
+  for(size_t i=0;i<n;i++) {
+    uint32_t a;
+    std::cin>>a;
+    pupils.push_back(a);
+  }
+
+  auto nastya=pupils.back();
+
+  std::unordered_map<uint32_t,std::unordered_set<uint32_t>> relations;
+  std::unordered_set<uint32_t> nastya_front_pupils;
+
+  for(size_t i=0;i<m;i++) {
+    uint32_t a,b;
+    std::cin>>a>>b;
+    if( b==nastya) {
+      nastya_front_pupils.emplace(a);
+    } else {
+      relations[a].insert(b);
+    }
+  }
+
+  size_t cnt=0;
+  auto last_it=pupils.rbegin()+1;
+  auto cur_it=last_it;
+  std::vector<uint32_t> front_pupils;
+  while(cur_it!=pupils.rend()) {
+    bool flag=true;
+    if (nastya_front_pupils.find(*cur_it)!=nastya_front_pupils.end()) {
+      auto const & relation= relations[*cur_it];
+      for(auto &pupil:front_pupils) {
+        if(relation.find(pupil)==relation.end()) {
+          flag=false;
+          break;
+        }
+      }
+      if(flag) {
+        cnt++;
+        std::swap(*cur_it,*last_it);
+        last_it++;
+      }
+    } else {
+      flag=false;
+    } 
+
+    if(!flag) {
+      front_pupils.push_back(*cur_it);
+    }
+    cur_it++;
+  }
+  std::cout<<cnt;
+  return 0;
+}
