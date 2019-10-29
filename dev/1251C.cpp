ec6f7fa7a8c25a93e088616b63a868f50cb00ba7
cyy <cyyever@outlook.com>
2019-10-29 13:47:32 +0800

    fix io

--- dev/1251C.cpp
+++ dev/1251C.cpp
@@ -18,37 +18,49 @@ int main() {
   std::vector<char> odd_chars;
   std::vector<char> even_chars;
 
-  for (size_t i = 0; i < n; i++) {
-    char a;
-    std::cin >> a;
-    if(a>='0'&& a<='9') {
-      if((a-'0')%2==0) {
-        even_chars.push_back(a);
-      } else {
-        odd_chars.push_back(a);
-      }
-    } else {
-      size_t i=0;
-      size_t j=0;
-      while(i<odd_chars.size()&&j<even_chars.size()) {
-        if(odd_chars[i]<even_chars[j]) {
-          std::cout<<odd_chars[i];
-          i++;
+  for (size_t n = 0; n < t; n++) {
+    size_t i = 0;
+    size_t j = 0;
+    while (true) {
+      int a=getchar();
+      bool is_digit = (a!=EOF&& a >= '0' && a <= '9');
+      if (is_digit) {
+        if ((a - '0') % 2 == 0) {
+          even_chars.push_back(a);
         } else {
-          std::cout<<even_chars[j];
-          j++;
+          odd_chars.push_back(a);
         }
       }
-      while(i<odd_chars.size()) {
-          std::cout<<odd_chars[i];
+      while (i < odd_chars.size() && j < even_chars.size()) {
+        if (odd_chars[i] < even_chars[j]) {
+          std::cout << odd_chars[i];
           i++;
-      }
-      while(j<even_chars.size()) {
-          std::cout<<even_chars[j];
+          if (i == odd_chars.size()) {
+            odd_chars.clear();
+            i = 0;
+          }
+        } else {
+          std::cout << even_chars[j];
           j++;
+          if (j == even_chars.size()) {
+            even_chars.clear();
+            j = 0;
+          }
+        }
+      }
+      if (!is_digit) {
+        for (auto c : odd_chars) {
+          std::cout << c;
+        }
+        odd_chars.clear();
+        for (auto c : even_chars) {
+          std::cout << c;
+        }
+        even_chars.clear();
+        break;
       }
     }
-    std::cout<<'\n';
+    std::cout << '\n';
   }
   return 0;
 }
